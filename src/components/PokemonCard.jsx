6abc16d9a938901e5f12efaab6bb82314805b772
Pat <hoang.patrick@outlook.com>
2023-10-09 16:45:26 +0200

    ajout données pokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,26 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
 function PokemonCard() {
+    let pokemon = pokemonList[1];
     return (
         <div>
             <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+                {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p>
+            }
             </figure>
             <figcaption>
-                Bulbasaur
+                {pokemon.name ? pokemon.name :<p>???</p>
+                }
             </figcaption>
         </div>
     )
